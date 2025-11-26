d7ad088ffb1fcf3c801a8856cd2bf9752db33f16
nordic96 <rhrlgns96@gmail.com>
2025-11-26 13:23:54 +0800

    removed unused components

--- app/components/PublicationSection.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import withHeaderDesc from "../functions/withHeaderDesc";
-
-function PublicationSection() {
-  return <></>;
-}
-
-export default withHeaderDesc(PublicationSection, "publication");
--- app/types/source.ts
+++
@@ -1,4 +0,0 @@
-export interface Source {
-  icon_url: string;
-  source_url: string;
-}
