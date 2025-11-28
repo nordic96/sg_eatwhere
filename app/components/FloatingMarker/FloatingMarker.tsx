3e2e12bf3ea82e70dd645d801ce22ad405cfd8c1
nordic96 <rhrlgns96@gmail.com>
2025-11-28 14:48:46 +0800

    added optimised floating marker HOC with ricebowl model

---
+++ app/components/FloatingMarker/FloatingMarker.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+// components/FloatingMarker.tsx
+import * as THREE from "three";
+import { ReactNode, useRef, useState } from "react";
+import { ThreeElements, useFrame } from "@react-three/fiber";
+import { Billboard, Html } from "@react-three/drei";
+import PlaceContent from "../PlaceContent/PlaceContent";
+import { FoodHeritage } from "@/app/constants/data";
+import { useRiceBowlModel } from "../markers/RiceBowlModel";
+
+type FloatingMarkerProps = ThreeElements["group"] & {
+  children: ReactNode;
+  data: FoodHeritage;
+  floatHeight?: number; // how high above the base model the indicator floats
+};
+
+export const FloatingMarker = ({
+  children,
+  position = [0, 0, 0],
+  floatHeight = 2,
+  data,
+  ...props
+}: FloatingMarkerProps) => {
+  const [open, setOpen] = useState(false);
+  const riceBowl = useRiceBowlModel();
+  const floatRef = useRef<THREE.Group>(null);
+
+  useFrame((state) => {
+    if (!floatRef.current) return;
+    const t = state.clock.elapsedTime;
+
+    floatRef.current.position.y = floatHeight + Math.sin(t * 1.4) * 0.3;
+    floatRef.current.rotation.y = Math.sin(t * 0.8) * 0.3;
+  });
+
+  return (
+    <group position={position} {...props}>
+      {/* Base object (e.g., SongFa, MBS) */}
+      <group>{children}</group>
+
+      {/* Floating indicator above */}
+      <group ref={floatRef} scale={1} onClick={() => setOpen(true)}>
+        <primitive object={riceBowl} />
+        <meshStandardMaterial emissive={"hotpink"} emissiveIntensity={2} toneMapped={false} />
+      </group>
+      {open && (
+        <Billboard position={[5, floatHeight + 10, 0]}>
+          <Html>
+            <div className={"flex w-[30vw] bg-white p-4 flex-col gap-2"}>
+              <div className="flex justify-end font-bold">
+                <button onClick={() => setOpen(false)}>X</button>
+              </div>
+              <PlaceContent data={data} />
+            </div>
+          </Html>
+        </Billboard>
+      )}
+    </group>
+  );
+};
---
+++ app/components/markers/RiceBowlModel.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useGLTF } from "@react-three/drei";
+import { useMemo } from "react";
+
+//TODO: Refactor in the future for more models such as Indian Food, Dessert 3D Models?
+export function useRiceBowlModel() {
+  const { scene } = useGLTF("/models/rice_bowl.glb");
+
+  return useMemo(() => scene.clone(), [scene]);
+}
+
+useGLTF.preload("/models/rice_bowl.glb");
