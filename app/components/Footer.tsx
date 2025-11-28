df12514514b447828adcb5a8c9d115d8f9d76628
nordic96 <rhrlgns96@gmail.com>
2025-11-28 18:37:01 +0800

    updated header footer styles and labels

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer>
-      <div className="flex grow px-8 py-4">{`© ${year} [Your Name] All Rights Reserved`}</div>
+    <footer className="bg-primary">
+      <div className="flex grow px-8 py-4">{`© ${year} Stephen Ko All Rights Reserved`}</div>
     </footer>
   );
 };
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import LabelContainer from "labelcontainer";
+import horizontalImage from "@/public/images/foodies_trail_sg_logo_horitontal.svg";
 
 const Header = () => {
   const lsInstance = LabelContainer.getInstance();
   return (
     <div>
       <header>
-        <div className="flex justify-between grow px-8 py-2">
-          <Link href={"/"}>{lsInstance.getLabel("header_title")}</Link>
+        <div className="flex items-center justify-between grow px-8 py-2 shadow-lg">
+          <div className="flex gap-4 items-center">
+            <div className="">
+              <Image src={horizontalImage} alt="logo" width={80} />
+            </div>
+            <Link href={"/"}>
+              <p className="font-semibold text-md">{lsInstance.getLabel("header_title")}</p>
+            </Link>
+          </div>
           <div>
             <Link href={"/about"}>About</Link>
           </div>
