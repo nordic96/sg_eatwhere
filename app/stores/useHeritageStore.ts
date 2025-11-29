e5e74d1365a023d01637f57224cea1d9b763d9cd
nordic96 <rhrlgns96@gmail.com>
2025-11-29 23:18:58 +0800

    added filter logics for useHeritageStore hook

--- app/stores/useHeritageStore.ts
+++ app/stores/useHeritageStore.ts
@@ -1,21 +1,28 @@
 /* eslint-disable no-unused-vars */
 import { create } from "zustand";
+import { EateryCategory } from "../types";
 
 type State = {
   heritageId: string | null;
+  filter: EateryCategory | null;
   clickedMore: boolean;
 };
 
 type Actions = {
   setHeritageId: (newId: string) => void;
+  setFilter: (cat: EateryCategory) => void;
+  unsetFilter: () => void;
   openMore: () => void;
   unSelect: () => void;
 };
 
 export const useHeritageStore = create<State & Actions>((set) => ({
   heritageId: null,
+  filter: null,
   clickedMore: false,
-  openMore: () => set({ clickedMore: true }),
   setHeritageId: (newId: string) => set({ heritageId: newId }),
+  setFilter: (newFilter: EateryCategory) => set({ filter: newFilter }),
+  unsetFilter: () => set({ filter: null }),
+  openMore: () => set({ clickedMore: true }),
   unSelect: () => set({ heritageId: null }),
 }));
