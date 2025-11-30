6fbd790bfc7409106c03d76ead6a12a7b0deff4c
nordic96 <rhrlgns96@gmail.com>
2025-11-30 17:40:27 +0800

    refactored heritageStore with sidebar logics

--- app/stores/useHeritageStore.ts
+++ app/stores/useHeritageStore.ts
@@ -13,18 +13,40 @@ type Actions = {
   setFilter: (cat: EateryCategory) => void;
   unsetFilter: (cat: EateryCategory) => void;
   openMore: () => void;
+  closeMore: () => void;
   unSelect: () => void;
 };
 
+export function openSidebar() {
+  const sidebar = document.getElementById("list-sidebar");
+  sidebar?.classList.remove("translate-x-full", "opacity-0");
+  sidebar?.classList.add("translate-x-0", "opacity-100");
+}
+
+export function closeSidebar() {
+  const toggleButton = document.getElementById("list-sidebar");
+  if (toggleButton !== null) {
+    toggleButton.classList.remove("translate-x-0", "opacity-100");
+    toggleButton.classList.add("translate-x-full", "optacity-0");
+  }
+}
+
 export const useHeritageStore = create<State & Actions>((set) => ({
   heritageId: null,
-  filter: [],
+  filter: ["hawker", "dessert", "restaurant"],
   clickedMore: false,
   setHeritageId: (newId: string) => set({ heritageId: newId }),
   setFilter: (newFilter: EateryCategory) =>
     set((state) => ({ filter: state.filter.concat([newFilter]) })),
   unsetFilter: (filter: EateryCategory) =>
     set((state) => ({ filter: state.filter.filter((x) => x !== filter) })),
-  openMore: () => set({ clickedMore: true }),
+  openMore: () => {
+    set({ clickedMore: true });
+    openSidebar();
+  },
+  closeMore: () => {
+    set({ clickedMore: false });
+    closeSidebar();
+  },
   unSelect: () => set({ heritageId: null }),
 }));
