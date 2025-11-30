5426ba0177854dd00ea0682220c7de916cff2280
nordic96 <rhrlgns96@gmail.com>
2025-11-30 19:58:46 +0800

    added updated card content ui component

--- app/components/PlaceContent/PlaceContent.tsx
+++ app/components/PlaceContent/PlaceContent.tsx
@@ -1,40 +1,92 @@
-import { FoodHeritage } from "@/app/constants/data";
-import Divider from "../Divider";
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-interface PlaceContentProps {
-  data: FoodHeritage;
-}
+import { useHeritageStore } from "@/app/stores";
+import { data as foodData } from "@/app/constants/data";
+import { MapOutlined, SubwayOutlined, ThumbUpOutlined } from "@mui/icons-material";
 
-export default function PlaceContent({ data }: PlaceContentProps) {
+import React, { useMemo } from "react";
+import { EateryCategory } from "@/app/types";
+import HighlightedText from "../HighlightText/HighlightText";
+import ImageCarousel from "../ImageCarousel/ImageCarousel";
+
+const CAT_ASSET_MAP: Record<EateryCategory, string> = {
+  hawker: "/images/hawker_bowl.svg",
+  restaurant: "/images/rooter_bowl.svg",
+  dessert: "/images/hawker_bowl.svg",
+};
+
+export default function PlaceContent() {
+  const { openMore } = useHeritageStore();
+  const heritageId = useHeritageStore((state) => state.heritageId);
+
+  const data = useMemo(() => {
+    return foodData.find((item) => item.id === heritageId) || null;
+  }, [heritageId]);
+
+  if (!heritageId || !data) {
+    return <div>No data selected</div>;
+  }
+
   return (
-    <div>
-      <div className={"relative h-50"}>
-        <Image className={"object-cover"} src={data.imgSource} alt={"img"} fill />
+    <div className="w-full flex flex-col gap-2">
+      {/** Carousel Container */}
+      <div className={"relative w-full h-[280px]"}>
+        <ImageCarousel img={data.imgSource} />
+        <span className="absolute bottom-0 left-[50%] translate-x-[-50%] text-center text-white">
+          <p className="text-4xl w-[200px] font-bold">{data.name}</p>
+          <p>{data.location.address}</p>
+        </span>
       </div>
-      <h3>{data.name}</h3>
-      <div>
-        <div className={"flex justify-between align-center"}>
-          <label>{data.location.address}</label>
-          <a href={data.location.gmapUrl} target={"_blank"}>
-            <div className={"h-4 w-4 relative"}>
-              <Image src={"/brands/gmap_icon.svg"} alt={"gmap"} fill />
-            </div>
-          </a>
-        </div>
+      {/** Info Container */}
+      <div className={"flex grow justify-start items-center gap-1 text-md"}>
+        <span className="flex gap-1 items-center">
+          <img className="h-6" src={CAT_ASSET_MAP[data.category]} alt={"icon"} />
+          <p>{data.category}</p>
+        </span>
+        <VerticalDivider />
+        <MrtLabel mrt={data.location.mrt[0]} />
+        <VerticalDivider />
+        <a href={data.location.gmapUrl} target="_blank">
+          <MapOutlined fontSize={"small"} />
+        </a>
+        <VerticalDivider />
+        <p>{"$$ 10 - 20 pax"}</p>
       </div>
-      <Divider />
-      <p>{data.desc}</p>
-      <Divider />
-      <div className={"flex flex-wrap gap-2 justify-center align-center"}>
-        {data.awards?.map((v, i) => {
-          return (
-            <div key={`award-${v}-${i}`} className={"relative h-14 w-14"}>
-              <Image src={`/brands/${v}.png`} fill alt={"award-logo"} />
-            </div>
-          );
-        })}
+      {/** Desc Container */}
+      <div className="flex flex-col mt-2 justify-center items-center gap-4">
+        <span className="flex justify-center items-center gap-1">
+          <ThumbUpOutlined />
+          <div>
+            {data.bestDish.map((dish, i) => (
+              <HighlightedText key={i}>{dish}</HighlightedText>
+            ))}
+          </div>
+        </span>
+        <p>{data.desc.substring(0, Math.min(data.desc.length, 100))}</p>
+      </div>
+      {/** Learn More Btn Container */}
+      <div className="flex justify-center">
+        <button
+          className="bg-primary py-1 px-4 rounded-lg text-white cursor-pointer"
+          onClick={openMore}
+        >
+          Learn More
+        </button>
       </div>
     </div>
+  );
+}
+
+function VerticalDivider() {
+  return <span className="border-r-[0.1px] border-[#555] h-6"></span>;
+}
+
+function MrtLabel({ mrt }: { mrt: string }) {
+  return (
+    <span className="flex gap-1 items-center">
+      <SubwayOutlined fontSize="small" />
+      <p className="font-public-sans font-medium">{mrt}</p>
+    </span>
   );
 }
