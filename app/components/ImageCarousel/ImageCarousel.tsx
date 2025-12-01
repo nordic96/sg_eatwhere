92ef910824f419a0203d94048a0aa8823a7f70f1
nordic96 <rhrlgns96@gmail.com>
2025-12-01 15:02:33 +0800

    updated image carousel component

--- app/components/ImageCarousel/ImageCarousel.tsx
+++ app/components/ImageCarousel/ImageCarousel.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { East, West } from "@mui/icons-material";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 
 interface ImageCarouselProps {
   img: string[];
@@ -10,6 +10,17 @@ interface ImageCarouselProps {
 
 export default function ImageCarousel({ img }: ImageCarouselProps) {
   const [currImg, setCurrImg] = useState<number>(0);
+
+  useEffect(() => {
+    function updatePosition() {
+      const el = document.getElementById("carousel-wrapper");
+      const offset = 100 / img.length;
+      if (el) {
+        el.style.transform = `translateX(${-currImg * offset}%)`;
+      }
+    }
+    updatePosition();
+  }, [currImg, img]);
 
   const onClickLeft = useCallback(() => {
     setCurrImg((index) => (index - 1 + img.length) % img.length);
@@ -20,8 +31,24 @@ export default function ImageCarousel({ img }: ImageCarouselProps) {
   }, [img]);
 
   return (
-    <div className="relative w-full h-full bg-monsoongrey">
-      <img className="w-full h-full object-cover" src={img[currImg]} alt={"main_photo"} />
+    <div className="w-full h-full relative bg-monsoongrey overflow-x-hidden">
+      <div
+        id="carousel-wrapper"
+        style={{ width: `${img.length * 100}%` }}
+        className="flex h-full transition-transform ease-in-out"
+      >
+        {img.map((src, i) => {
+          return (
+            <img
+              key={i}
+              style={{ width: `${100 / img.length}%` }}
+              className={"h-full object-cover"}
+              src={src}
+              alt={"main_photo"}
+            />
+          );
+        })}
+      </div>
       <div
         className="absolute left-0 top-[50%] opacity-80 text-white bg-primary rounded-3xl cursor-pointer"
         onClick={onClickLeft}
