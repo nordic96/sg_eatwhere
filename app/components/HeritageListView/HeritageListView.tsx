187b07455f3674f000bc498d6a3b238bc46615da
nordic96 <rhrlgns96@gmail.com>
2025-12-01 18:13:32 +0800

    added heritage list view component

---
+++ app/components/HeritageListView/HeritageListView.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { CAT_ASSET_MAP, data } from "@/app/constants/data";
+import { useHeritageStore } from "@/app/stores";
+import { Region } from "@/app/types";
+import { useMemo } from "react";
+import HighlightedText from "../HighlightText/HighlightText";
+
+interface HeritageListViewProps {
+  region: Region;
+}
+
+export default function HeritageListView({ region }: HeritageListViewProps) {
+  const { heritageId, setHeritageId } = useHeritageStore();
+  const heritageDataByRegion = useMemo(() => {
+    return data.filter((v) => v.location.region === region);
+  }, [region]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="font-medium text-[16px]">{`${region} Region`}</label>
+      <div className="flex flex-col gap-2">
+        {heritageDataByRegion.map((location) => {
+          return (
+            <div
+              key={`list-view-${location.id}`}
+              className="flex items-center gap-1 cursor-pointer"
+              onClick={() => setHeritageId(location.id)}
+            >
+              <img src={CAT_ASSET_MAP[location.category]} className={"h-8"} alt={"category-icon"} />
+              {location.id === heritageId ? (
+                <HighlightedText>
+                  <label className="text-sm">{location.name}</label>
+                </HighlightedText>
+              ) : (
+                <label className="text-sm cursor-pointer">{location.name}</label>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
