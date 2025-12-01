2e7d7944081b5ce8f5184e939758d55d1a4d5653
nordic96 <rhrlgns96@gmail.com>
2025-12-01 18:13:20 +0800

    added region to heritagetype

--- app/constants/data.ts
+++ app/constants/data.ts
@@ -22,6 +22,7 @@ export const data: FoodHeritage[] = [
       address: "76 Serangoon Rd, Singapore 217981",
       gmapUrl: "https://maps.app.goo.gl/k7tFGxx6g8q9ooL49",
       mrt: ["Little India"],
+      region: "central",
       geoLocation: {
         latitude: 1.307901,
         longitude: 103.853522,
@@ -44,6 +45,7 @@ export const data: FoodHeritage[] = [
       address: "7 Maxwell Rd, Amoy St, Food Centre, 02-119, 069111",
       gmapUrl: "https://maps.app.goo.gl/hmquZSaXGugMDH4q8",
       mrt: ["Tanjong Pagar"],
+      region: "central",
       geoLocation: {
         latitude: 1.2792477058866272,
         longitude: 103.84652104460396,
@@ -66,6 +68,7 @@ export const data: FoodHeritage[] = [
       address: "190 Middle Rd, #02-08 Fortune Centre, Singapore 188979",
       gmapUrl: "https://maps.app.goo.gl/kAc8EuW5MqgZDbWQA",
       mrt: ["Bencoolen"],
+      region: "central",
       geoLocation: {
         latitude: 1.3007023528612611,
         longitude: 103.85208220385952,
@@ -88,6 +91,7 @@ export const data: FoodHeritage[] = [
       address: "462 Crawford Ln, #01-69/71, Singapore 190462",
       gmapUrl: "https://maps.app.goo.gl/mBPhoS7j188iqBzv9",
       mrt: ["Lavender", "Bugis", "Nicoll Highway"],
+      region: "central",
       geoLocation: {
         latitude: 1.3050929262076814,
         longitude: 103.86270769767101,
@@ -108,6 +112,7 @@ export const data: FoodHeritage[] = [
       address: "1 Everton Park, #01-33, Singapore 081001",
       gmapUrl: "https://maps.app.goo.gl/SJmoZAJ9NkfEEoau6",
       mrt: ["Outram Park", "Tanjong Pagar"],
+      region: "central",
       geoLocation: {
         latitude: 1.277494311833965,
         longitude: 103.84025398917902,
@@ -130,6 +135,7 @@ export const data: FoodHeritage[] = [
       address: "1 Teo Hong Rd, Singapore 088321",
       gmapUrl: "https://maps.app.goo.gl/gqcUMstgvKDpAsZ7A",
       mrt: ["Outram Park", "Tanjong Pagar"],
+      region: "central",
       geoLocation: {
         latitude: 1.280484583904037,
         longitude: 103.84097249373312,
