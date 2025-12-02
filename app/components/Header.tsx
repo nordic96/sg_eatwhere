ae57efb7040613fe2c5104813e9eb8405532b971
nordic96 <rhrlgns96@gmail.com>
2025-12-02 19:41:41 +0800

    added localeswitcher in header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import Link from 'next/link';
+'use client';
+
+import React, { useRef } from 'react';
 import Image from 'next/image';
 import horizontalImage from '@/public/images/header_icon.svg';
+import { Translate } from '@mui/icons-material';
+import { useLocale } from 'next-intl';
+import Link from 'next/link';
+import LocaleSwitcher from './LocaleSwitcher/LocaleSwitcher';
+import useLangNames from '@/i18n/useLangNames';
+import { useAppStore } from '../stores';
+import useClickOutside from '../hooks/useClickOutside';
+import { LocaleIconMap } from '../constants/localeIconMap';
 
 const Header = () => {
+  const currentLocale = useLocale();
+  const ref = useRef(null);
+  const { localeOpen, openLocale, closeLocale } = useAppStore();
+  const localeNames = useLangNames(currentLocale);
+  useClickOutside(ref, closeLocale);
+
   return (
     <div>
       <header className="bg-primary text-white flex justify-center">
@@ -15,8 +30,27 @@ const Header = () => {
               </Link>
             </div>
           </div>
-          <div>
-            <Link href={'/about'}>About</Link>
+          <div className="relative w-[100px]">
+            <div className="flex gap-4 justify-between items-center">
+              <Translate fontSize="small" />
+              <div className="text-sm items-center flex flex-1">
+                <span className={LocaleIconMap[currentLocale]}></span>
+                <label onClick={openLocale} className="text-sm p-1 cursor-pointer">
+                  {localeNames.of(currentLocale)}
+                </label>
+              </div>
+            </div>
+            {localeOpen && (
+              <div
+                ref={ref}
+                id="locale-swticher"
+                className="absolute shadow-xl border-[#333] border top-[38px] right-0 z-999 bg-[#f3f3f3] text-black rounded-b-lg w-[116px] flex flex-col p-2 gap-1"
+              >
+                <LocaleSwitcher locale={'en'} />
+                <LocaleSwitcher locale={'ko'} />
+                <LocaleSwitcher locale={'ja'} />
+              </div>
+            )}
           </div>
         </div>
       </header>
