fa9f7c20f047bf9e2794e996c6adabdfe04aae0f
nordic96 <rhrlgns96@gmail.com>
2025-12-02 19:36:11 +0800

    refactored geti18nConfig to load eng as fallback

--- i18n/request.ts
+++ i18n/request.ts
@@ -4,10 +4,17 @@ import { cookies } from 'next/headers';
 export async function geti18nConfig() {
   const store = await cookies();
   const locale = store.get('locale')?.value || 'en';
+  let message;
+  try {
+    message = (await import(`@/messages/${locale}.json`)).default;
+  } catch (e) {
+    console.error(e);
+    message = (await import('@/messages/en.json')).default;
+  }
 
   return {
     locale,
-    messages: (await import(`@/messages/${locale}.json`)).default,
+    messages: message,
   };
 }
 
