32561849c8066ee60788cfd5e7fc18d5816773c4
nordic96 <rhrlgns96@gmail.com>
2025-12-03 22:48:36 +0800

    added jsdelivr cdn to domain in nextconfig

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [],
+    domains: ['cdn.jsdelivr.net'],
   },
 };
 
