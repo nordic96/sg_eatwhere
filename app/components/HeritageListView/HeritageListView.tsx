db7db1d06c4521b7a8aa987a56923ff4c4055d32
nordic96 <rhrlgns96@gmail.com>
2025-12-03 09:25:34 +0800

    applied filter in listview component and disabled unsetting filter to none

--- app/components/HeritageListView/HeritageListView.tsx
+++ app/components/HeritageListView/HeritageListView.tsx
@@ -13,7 +13,7 @@ interface HeritageListViewProps {
 }
 
 export default function HeritageListView({ region }: HeritageListViewProps) {
-  const { heritageId, setHeritageId } = useHeritageStore();
+  const { heritageId, setHeritageId, filter } = useHeritageStore();
   const t = useTranslations('HeritageListView');
   const heritageDataByRegion = useMemo(() => {
     return data.filter((v) => v.location.region === region);
@@ -23,29 +23,31 @@ export default function HeritageListView({ region }: HeritageListViewProps) {
     <div className="flex flex-col gap-2">
       <label className="font-medium text-[14px]">{t(region)}</label>
       <div className="flex flex-col gap-2">
-        {heritageDataByRegion.map((location) => {
-          return (
-            <div
-              key={`list-view-${location.id}`}
-              className="flex items-center gap-1 cursor-pointer"
-              onClick={() => setHeritageId(location.id)}
-            >
-              <img
-                src={CAT_ASSET_MAP[location.category]}
-                className={'h-8'}
-                alt={'category-icon'}
-                draggable="false"
-              />
-              {location.id === heritageId ? (
-                <HighlightedText>
-                  <label className="text-[12px]">{location.name}</label>
-                </HighlightedText>
-              ) : (
-                <label className="text-[12px] cursor-pointer">{location.name}</label>
-              )}
-            </div>
-          );
-        })}
+        {heritageDataByRegion
+          .filter((val) => filter.includes(val.category))
+          .map((location) => {
+            return (
+              <div
+                key={`list-view-${location.id}`}
+                className="flex items-center gap-1 cursor-pointer"
+                onClick={() => setHeritageId(location.id)}
+              >
+                <img
+                  src={CAT_ASSET_MAP[location.category]}
+                  className={'h-8'}
+                  alt={'category-icon'}
+                  draggable="false"
+                />
+                {location.id === heritageId ? (
+                  <HighlightedText>
+                    <label className="text-[12px]">{location.name}</label>
+                  </HighlightedText>
+                ) : (
+                  <label className="text-[12px] cursor-pointer">{location.name}</label>
+                )}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
