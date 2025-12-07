386eaf10d7d01995452020fa7f1d5656167ee4e7
nordic96 <rhrlgns96@gmail.com>
2025-12-07 16:34:49 +0800

    added hero section for mobile responsive views

---
+++ app/constants/index.ts
@@ -0,0 +1,3 @@
+export * from './theme';
+export * from './localeIconMap';
+export * from './data';
--- app/constants/theme.ts
+++ app/constants/theme.ts
@@ -8,3 +8,7 @@ export const ThemeRecord: Record<EateryCategory, ThemeColor> = {
 
 export const baseLayoutStyle =
   'max-w-[1440px] max-sm:max-w-[100vw] xl:px-8 lg:px-4 max-sm:px-4 md:px-4';
+
+export const h1BaseStyle =
+  'text-5xl max-sm:text-4xl whitespace-pre-line font-extrabold leading-none';
+export const microTextBaseStyle = 'font-light text-xl max-sm:text-lg';
