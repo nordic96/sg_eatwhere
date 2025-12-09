37d30df4720ee677cdd1f3f90c31914c151f551e
nordic96 <rhrlgns96@gmail.com>
2025-12-09 23:03:26 +0800

    handle network error for fetch

--- app/types/DataResponse.ts
+++ app/types/DataResponse.ts
@@ -1,4 +1,9 @@
-export interface DataResponse<T> {
-  data: T;
+export interface ErrorResponse {
   error?: string | Error;
 }
+
+export interface DataResponse<T> {
+  data: T | null;
+}
+
+export type AppResponse<T> = DataResponse<T> & ErrorResponse;
