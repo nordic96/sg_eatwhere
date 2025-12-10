d1d231348ecfccccdda68979099ab1dae70d135b
nordic96 <rhrlgns96@gmail.com>
2025-12-10 14:55:33 +0800

    asset loading debugger mode in devmode

--- app/hooks/useInstancingModel.ts
+++ app/hooks/useInstancingModel.ts
@@ -1,12 +1,19 @@
 import * as THREE from 'three';
 import { useGLTF } from '@react-three/drei';
 import { useMemo } from 'react';
+import { isProductionMode } from '../utils';
 
 export function useInstancingModel(path: string) {
   const { scene } = useGLTF(path);
 
   return useMemo(() => {
     const mesh = scene.children[0] as THREE.Mesh;
+    if (!isProductionMode()) {
+      return {
+        geometry: new THREE.BoxGeometry(2, 3, 2),
+        material: new THREE.MeshBasicMaterial({ color: 'black' }),
+      };
+    }
     return {
       geometry: mesh.geometry,
       material: mesh.material,
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -2,3 +2,4 @@ export * from './cn';
 export * from './geographyUtil';
 export * from './calcEnvironment';
 export * from './networkUtils';
+export * from './devUtils';
