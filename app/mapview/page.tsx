30095b5cbd835da3a6ae5cfa0ba16e051a9979d1
nordic96 <rhrlgns96@gmail.com>
2025-12-14 08:32:20 +0800

    dev mode to fetch test data

--- app/mapview/page.tsx
+++ app/mapview/page.tsx
@@ -1,11 +1,21 @@
 import { geti18nConfig } from '@/i18n/request';
 import ClientHome from './ClientHome';
-import { FoodHeritage } from '../types';
+import { AppResponse, FoodHeritage } from '../types';
 import { fetchApi } from '../utils';
+import data from '@/resources/devData.json';
 
 export default async function Page() {
   const { messages, locale } = await geti18nConfig();
-  const response = await fetchApi<FoodHeritage[]>(process.env.DATA_URL || '');
+  let response: AppResponse<FoodHeritage[]> = { data: [] };
+  if (process.env.USE_TEST_DATA === 'true') {
+    response = await new Promise((res) => {
+      setTimeout(() => {
+        res({ data: JSON.parse(JSON.stringify(data)) });
+      }, 2000);
+    });
+  } else {
+    response = await fetchApi<FoodHeritage[]>(process.env.DATA_URL || '');
+  }
   return (
     <ClientHome
       messages={messages}
