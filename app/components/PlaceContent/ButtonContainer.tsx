0d81ea7b72d006bf34e5e885ac59613d60ac4c60
nordic96 <rhrlgns96@gmail.com>
2025-12-14 22:07:57 +0800

    refactored place content button container with trail next/prev button

---
+++ app/components/PlaceContent/ButtonContainer.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useHeritageStore } from '@/app/stores';
+import { Redo, Undo } from '@mui/icons-material';
+import { Activity } from 'react';
+import CloseButton from '../CloseButton/CloseButton';
+import { cn } from '@/app/utils';
+
+export default function ButtonContainer() {
+  const { unSelect, getThemeStyle, trailMode, moveToNextTrail } = useHeritageStore();
+  const trailButtonStyle =
+    'h-6 w-6 bg-primary text-white hover:bg-red-700 rounded-full cursor-pointer flex items-center justify-center';
+  return (
+    <div className={'flex w-full justify-between items-center'}>
+      <div className={'flex gap-2'}>
+        <Activity mode={trailMode ? 'visible' : 'hidden'}>
+          <div className={cn(trailButtonStyle)} onClick={() => moveToNextTrail(true)}>
+            <Undo fontSize={'inherit'} />
+          </div>
+          <div className={cn(trailButtonStyle)} onClick={() => moveToNextTrail()}>
+            <Redo fontSize={'inherit'} />
+          </div>
+        </Activity>
+      </div>
+      <CloseButton onClick={unSelect} customClass={getThemeStyle()} />
+    </div>
+  );
+}
