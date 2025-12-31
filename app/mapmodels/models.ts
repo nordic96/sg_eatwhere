0977a7f92d743f576f792c415b6b96307fc032da
nordic96 <rhrlgns96@gmail.com>
2025-12-31 15:47:13 +0800

    refactored models with cdn base

--- app/mapmodels/models.ts
+++ app/mapmodels/models.ts
@@ -3,11 +3,12 @@
 import { useGLTF } from '@react-three/drei';
 import { isProductionMode } from '../utils';
 
+const CDN_BASE = process.env.NEXT_PUBLIC_CDN_BASE || '';
 export const MODELS = {
-  riceBowl: '/models/rice_bowl.glb',
-  hawkerStall: '/models/hawker_stall_opt.glb',
-  shophouse: '/models/shophouse_A_opt.glb',
-  locationpin: '/models/location_pin.glb',
+  riceBowl: `${CDN_BASE}/resources/models/rice_bowl.glb`,
+  hawkerStall: `${CDN_BASE}/resources/models/hawker_stall_opt.glb`,
+  shophouse: `${CDN_BASE}/resources/models/shophouse_A_opt.glb`,
+  locationpin: `${CDN_BASE}/resources/models/location_pin.glb`,
 };
 
 export function useModel(name: keyof typeof MODELS) {
