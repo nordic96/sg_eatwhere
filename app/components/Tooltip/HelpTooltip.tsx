5d48fb63a035c16060c7cdc564e9ad7ba0b7e2e1
nordic96 <rhrlgns96@gmail.com>
2026-01-20 08:31:12 +0800

    removed unused iconprops in HelpTooltip.tsx

--- app/components/Tooltip/HelpTooltip.tsx
+++ app/components/Tooltip/HelpTooltip.tsx
@@ -1,18 +1,16 @@
 import { FiHelpCircle } from 'react-icons/fi';
 import Tooltip, { TooltipProps } from './Tooltip';
 import { useTranslations } from 'next-intl';
-import { IconType } from 'react-icons';
 
 type HelpTooltipProps = Omit<TooltipProps, 'content'> & {
   msgKey: string;
-  iconProps?: IconType;
 };
 
-export default function HelpTooltip({ msgKey, iconProps, ...tooltipProps }: HelpTooltipProps) {
+export default function HelpTooltip({ msgKey, ...tooltipProps }: HelpTooltipProps) {
   const tooltipT = useTranslations('Tooltip');
   return (
     <Tooltip {...tooltipProps} content={tooltipT(msgKey)}>
-      <FiHelpCircle size={18} {...iconProps} />
+      <FiHelpCircle size={18} />
     </Tooltip>
   );
 }
