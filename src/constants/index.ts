400e49165b0e8d121f5f739de2e6b21961956594
nordic96 <rhrlgns96@gmail.com>
2026-05-13 13:54:57 +0800

    Response types and new MRT properties changes * i18n now supports with new env var 'USE_TEST_LOCALE' to redirect backup locales for dev mode * en.json locales for spicy related locales * mrtMap.json added for new `mrt_codes` support; `mrt` property is now deprecated * appConfig to fetch env var `USE_TEST_LOCALE` * mrtLabelMap to import mrt label map from json and export for ts use * styles/mrt for MRT line related colour code styles * type/foodHeritage for new spicy, mrt_codes property support * updated devData.json with most recent UAT data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './theme';
 export * from './localeIconMap';
 export * from './canvasConstants';
+export * from './mrtLabelMap';
---
+++ src/constants/mrtLabelMap.ts
@@ -0,0 +1,3 @@
+import mrtMap from '@/resources/mrtMap.json';
+
+export const mrtLabelMap = mrtMap as Record<string, string>;
